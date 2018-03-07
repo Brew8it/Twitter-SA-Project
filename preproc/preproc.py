715ba2386a8f2e855d6837725cb584800971dc0c
Brew8it <bandgren@live.se>
2018-03-07 13:48:09 +0100

    added support for expanding contractions

--- preproc/preproc.py
+++ preproc/preproc.py
@@ -1,11 +1,14 @@
 # coding=utf-8
 import os
+import re
 import pandas as pd
 from miner import miner
 from bs4 import BeautifulSoup
 from nltk.corpus import stopwords
 from nltk import word_tokenize
 from nltk.stem.snowball import SnowballStemmer
+
+import numpy as np
 import nltk
 from nltk.tokenize.toktok import ToktokTokenizer
 
@@ -20,6 +23,10 @@ stemmer = SnowballStemmer('english')
 stop = stopwords.words('english')
 stop.remove('not')
 
+# for expanding contractions
+
+cont_dict = {'can\'t': 'cannot', 'won\'t': 'will not', 'n\'t': ' not'}
+cont_re = re.compile('(%s)' % '|'.join(cont_dict.keys()))
 
 
 class preProc(object):
@@ -29,19 +36,30 @@ class preProc(object):
     def loadCsv(self, path, name):
         os.chdir(path)
         self.df = pd.read_csv(name, sep=",").dropna()
-        self.df.columns = ["num","lable", "tweet"]
+        self.df.columns = ["num", "lable", "tweet"]
 
     def loadOwnDataFrame(self, dataframe):
         self.df = dataframe.dropna()
 
-    def clean_data(self, html_strpping=True, accented_char_removal=True, to_lower=True, remove_links=True, remove_mentions=True,
-                   remove_hashtag=True, remove_extra_whitespace=True, tokenize=True, stemming=True, remake_document=True):
+    def expand_cont(self, s, cont_dict=cont_dict):
+        def replace(match):
+            return cont_dict[match.group(0)]
+
+        return cont_re.sub(replace, s)
+
+    def clean_data(self, html_strpping=True, accented_char_removal=True, to_lower=True, remove_links=True,
+                   remove_mentions=True,
+                   remove_hashtag=True, remove_extra_whitespace=True, tokenize=True, stemming=True,
+                   remake_document=True,
+                   expand_contractions=True):
         if html_strpping:
             self.remove_html_encode()
-        if accented_char_removal:
-            self.remove_accented_chars()
         if to_lower:
             self.to_lower()
+        if expand_contractions:
+            self.expand_contractions()
+        if accented_char_removal:
+            self.remove_accented_chars()
         if remove_links:
             self.remove_links()
         if remove_mentions:
@@ -68,7 +86,8 @@ class preProc(object):
         self.df["tweet"] = self.df["tweet"].apply(lambda x: BeautifulSoup(x, 'lxml').get_text())
 
     def remove_accented_chars(self):
-        self.df["tweet"] = self.df["tweet"].apply(lambda x: unicodedata.normalize('NFKD', x).encode('ascii', 'ignore').decode('utf-8', 'ignore'))
+        self.df["tweet"] = self.df["tweet"].apply(
+            lambda x: unicodedata.normalize('NFKD', x).encode('ascii', 'ignore').decode('utf-8', 'ignore'))
 
     def to_lower(self):
         self.df["tweet"] = self.df["tweet"].str.lower()
@@ -85,6 +104,9 @@ class preProc(object):
     def remove_extra_whitepsace(self):
         self.df.loc[:, "tweet"] = self.df.loc[:, "tweet"].replace(' +', ' ', regex=True)
 
+    def expand_contractions(self):
+        self.df["tweet"] = self.df["tweet"].apply(lambda x: self.expand_cont(x))
+
     def tokenize(self):
         # self.df["tweet"] = self.df.apply(lambda row: word_tokenize(row["tweet"]), axis=1) bajs lambda
         self.df["tweet"] = self.df["tweet"].apply(word_tokenize)
@@ -99,19 +121,22 @@ class preProc(object):
         self.df["tweet"] = self.df["tweet"].apply(lambda x: ' '.join(x))
 
 
-
-
-
 # for testing
 
 if __name__ == "__main__":
     test = preProc()
-    test.loadCsv("../datasets/STS/", "STS_test.csv")
-    #test.loadCsv("../datasets/SemEval/4A-English/", "SemEval.csv")
+    # test.loadCsv("../datasets/STS/", "STS_test.csv")
+    # test.loadCsv("../datasets/SemEval/4A-English/", "SemEval.csv")
 
+    df = pd.DataFrame(
+        np.array([[1, "you don\'t need didn\'t"], [2, "This is a test tweet with you don\'t need didn\'t"]]))
+    df.columns = ["lable", "tweet"]
+    test.loadOwnDataFrame(df)
     test.clean_data()
     df = test.get_twitter_df()
     print(df)
+
+    """
     test = preProc()
     tweet_miner = miner.TwitterMiner()
     # tweet_miner.collect_tweets_from_searching("#svpol", 10)
@@ -120,3 +145,4 @@ if __name__ == "__main__":
     test.clean_data()
     df = test.get_twitter_df()
     print(df)
+    """
--- testning/test.py
+++ testning/test.py
@@ -1,18 +1,35 @@
 import re
 
+import pandas as pd
+import numpy as np
 
-cont_dict = {'didn\'t': 'did not', 'don\'t' : 'do not',}
+cont_dict = {'can\'t': 'cannot', 'won\'t': 'will not', 'n\'t': ' not'}
 cont_re = re.compile('(%s)' % '|'.join(cont_dict.keys()))
+
 
 def expand_cont(s, cont_dict=cont_dict):
     def replace(match):
         return cont_dict[match.group(0)]
+
     return cont_re.sub(replace, s)
 
-print(expand_cont('you don\'t need didn\'t'))
+
+# print(expand_cont('you don\'t need didn\'t'))
+
+df = pd.DataFrame(np.array([[1, "you don\'t need didn\'t :: can\'t, won\'t"], [2, "This is a test tweet with you don\'t need didn\'t"]]))
+
+df[1] = df[1].apply(lambda x: expand_cont(x))
+
+print(df)
+
+
+
+"""
 
 self.df["tweet"] = self.df["tweet"].apply(lambda x: [item for item in x if item not in stop])
 
 self.df["tweet"] = self.df["tweet"].apply(lambda x: [stemmer.stem(y) for y in x])
 
 if x 
+
+"""
