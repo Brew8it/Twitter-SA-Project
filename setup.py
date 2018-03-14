ea0965de7973e99bbe59881891b68cd743cfb013
Brew8it <bandgren@live.se>
2018-03-14 09:36:38 +0100

    added venv

--- setup.py
+++ setup.py
@@ -1,12 +1,22 @@
-from TSA.Preproc import Preproc
-from TSA.NB import NB
-from TSA.SVM import SVM
-from TSA.Prediction import Prediction
-from TSA.GUI import app
+from setuptools import setup
 
-# Preproc.main()
-# NB.train_NB()
-# SVM.train_SVM()
-# Prediction.main()
-app.main()
+setup(name='TSA',
+      version='0.1',
+      description='Graduate work for CGI on Twitter Sentiment analysis',
+      url='https://github.com/Brew8it/Twitter-SA-Project',
+      author='Brew8it and Selberget',
+      author_email='bandgren2@gmail.com & johan.selberg@gmail.com',
+      license='MIT',
+      packages=['TSA'],
+      install_requires=[
+                        'NumPy',
+                        'SciPy',
+                        'scikit-learn',
+                        'pandas',
+                        'nltk',
+                        'tweepy',
+                        'bs4',
+                        'flask',
 
+      ],
+      zip_safe=False)
