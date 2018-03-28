48b2202536242ab2579d1ef88f3ec515663b51e7
Brew8it <bandgren@live.se>
2018-03-28 13:07:58 +0200

    made 2nd split SVM STS

--- SVM_gridsearch_STS.py
+++ SVM_gridsearch_STS.py
@@ -24,18 +24,18 @@ target_names = ['Positive', 'Negative']
 # SemEval vocab size 10272
 
 pipeline = Pipeline([
-    ('vect', CountVectorizer()),
-    #('kbest', SelectKBest(chi2)),
-    ('tfidf', TfidfTransformer()),
+    ('vect', CountVectorizer(max_df=0.5, ngram_range=(1, 3))),
+    ('kbest', SelectKBest(chi2)),
+    ('tfidf', TfidfTransformer(norm='none', use_idf=True)),
     ('clf', SGDClassifier()),
 ])
 
 parameters = {
-    'vect__max_df': (0.5, 0.75, 1.0),
-    'vect__ngram_range': ((1, 1), (1, 2), (1, 3)),  # unigrams or bigrams
-    'tfidf__use_idf': (True, False),
-    'tfidf__norm': (None, 'l1', 'l2'),
-    #'kbest__k': (30000, 50000, 100000, 130000, 'all'),
+    #'vect__max_df': (0.5, 0.75, 1.0),
+    #'vect__ngram_range': ((1, 1), (1, 2), (1, 3)),  # unigrams or bigrams
+    #'tfidf__use_idf': (True, False),
+    #'tfidf__norm': (None, 'l1', 'l2'),
+    'kbest__k': (30000, 50000, 100000, 130000, 'all'),
     #'clf__alpha': (1e-4, 1e-3, 1e-2, 1e-1, 1e0, 1e1),
     #'clf__max_iter': (500, 1000, 1500),
     #'clf__penalty': ('l2', 'l1', 'elasticnet'),
